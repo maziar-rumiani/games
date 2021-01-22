aa1804480efcec808adc9694f947c79f0f412e3b
Rumiani <maziar9170@gmail.com>
2021-01-22 06:12:23 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var law='Click to jump and save the girl from angry man! :) ,So... if you are ready press "ok"';
 alert(law);
+const yellgroan=new Audio('yellgroan.mp3');
+const girl=new Audio('girl.mp3');
+const Scream=new Audio('Scream.mp3');
+
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 var counter=0;
@@ -9,17 +13,22 @@ function jump(){
     setTimeout(function(){
         character.classList.remove("animate");
     },300);
+    girl.play();
 }
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
         block.style.animation = "none";
-        alert("Game Over. score: ("+Math.floor(counter/100)+')---If you wanna play againpress "ok"');
+        alert("Game Over. score:  ("+Math.floor(counter/100)+')         If you wanna play againpress "ok"'+Scream.play());
         counter=0;
         block.style.animation = "block 1.5s infinite linear";
+
     }else{
         counter++;
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
-    }
+        yellgroan.play();
+
+    }         
+
 }, 10);
